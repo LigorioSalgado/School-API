6ee9f4e052cd2b84a8c81fb5fde2c8baa630df78
Ligorio Edwin Salgado Flores <edwinsalgado@Mac-mini-de-Ligorio.local>
2021-06-10 14:22:01 -0500

    chnages for lowdb  version
    Signed-off-by: Ligorio Edwin Salgado Flores <edwinsalgado@Mac-mini-de-Ligorio.local>

--- models/Alumnos.js
+++ models/Alumnos.js
@@ -4,26 +4,23 @@ const FileSync = require('lowdb/adapters/FileSync')
 class Alumno{
 
     constructor(){
-        
-        const adapter = new FileSync(file);
+        const adapter = new FileSync('db.json');
         const db = low(adapter)
         this.db = db;
     }
 
     async getAlumnos(){
-        await this.db.read(); //va a leer los ultimos datos en la BD
-        return this.db.data.alumnos;
+         //va a leer los ultimos datos en la BD
+        return this.db.get('alumnos').value();
     }
 
     async addAlumno(alumno){
-        this.db.data.alumnos.push(alumno)
-        await this.db.write(); // esto modifica el db.json
+        this.db.get('alumnos').push(alumno).write()
         return alumno
     }
 
     async getAlumnoByEmail(email){
-        await this.db.read();
-        const alumno = this.db.data.alumnos.find(student => student.email === email);
+        const alumno = this.db.get('alumnos').value().find(student => student.email === email);
         return alumno
     }
 
